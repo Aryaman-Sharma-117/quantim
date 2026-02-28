dd0205fdc2b0b8f774daba8a0456e810364ecee8
Aryaman Sharma <aryamansharma.india@gmail.com>
2026-03-01 05:00:22 +0530

    task-4

--- app.py
+++ app.py
@@ -1,8 +1,4 @@
-# Run this app with `python app.py` and
-# visit http://127.0.0.1:8050/ in your web browser.
-
-
-from dash import Dash, dcc, html
+from dash import Dash, dcc, html, Input, Output, callback
 import plotly.express as px
 import pandas as pd
 
@@ -14,14 +10,8 @@ colors = {
 }
 
 df = pd.read_csv("output.csv")
-
-fig = px.line(df, x="Date", y="Sales", title='Pink Morsel Sales Over Time')
-
-fig.update_layout(
-    plot_bgcolor=colors['background'],
-    paper_bgcolor=colors['background'],
-    font_color=colors['text']
-)
+df["Date"] = pd.to_datetime(df["Date"])
+df = df.sort_values("Date")
 
 app.layout = html.Div(style={'backgroundColor': colors['background']}, children=[
     html.H1(
@@ -38,10 +28,47 @@ app.layout = html.Div(style={'backgroundColor': colors['background']}, children=
     }),
 
     dcc.Graph(
-        id='example-graph-2',
-        figure=fig
-    )
+        id='example-graph-2'
+    ),
+    html.Div(children='Select a region:', style={
+        'textAlign': 'center',
+        'color': colors['text']
+    }),
+    html.Div(style={'textAlign': 'center'}, children=[
+        dcc.RadioItems(
+            ['north', 'south', 'east', 'west'],
+            'north',
+            id='region-radio',
+            inline=True,
+            labelStyle={
+                'color': colors['text'],
+                'textAlign': 'center',
+                'margin-right': '15px'
+            }
+        )
+    ])
+        
+    
 ])
+
+@callback(
+    Output('example-graph-2', 'figure'),
+    Input('region-radio', 'value')
+)
+def update_figure(selected_region):
+    filtered_df = df[df['Region'] == selected_region]
+
+    fig = px.line(filtered_df, x="Date", y="Sales",
+                  title='Pink Morsel Sales Over Time')
+
+    fig.update_layout(
+        plot_bgcolor=colors['background'],
+        paper_bgcolor=colors['background'],
+        font_color=colors['text'],
+    )
+
+    return fig
+
 
 if __name__ == '__main__':
     app.run(debug=True)
